5a5fd8103a4afe2c63ef263af85d29890d900600
SatisKia <satis@msd.biglobe.ne.jp>
2022-02-09 00:06:46 +0900

    fix

--- lib/flutter_first_plugin.dart
+++ lib/flutter_first_plugin.dart
@@ -1,9 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/services.dart';
 
 class FlutterFirstPlugin {
   static const MethodChannel _channel = MethodChannel('flutter_first_plugin');
 
-  static void vibrate(){
-    _channel.invokeMethod( 'vibrate' );
+  static Future vibrate() async {
+    await _channel.invokeMethod( 'vibrate' );
   }
 }
